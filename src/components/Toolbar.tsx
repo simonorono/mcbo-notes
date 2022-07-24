529801231de364017d3de34118e03a5e98bd0218
Simón Oroño <simonorono@protonmail.com>
2022-07-24 13:06:49 -0400

    Increase thickness of toolbar separator

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -8,7 +8,7 @@ export default function Toolbar(props: Props) {
   const { children } = props
 
   return (
-    <div className="flex justify-end space-x-4 border-b border-black px-4 py-2">
+    <div className="flex justify-end space-x-4 border-b border-b-2 border-black px-4 py-2">
       {children}
     </div>
   )
