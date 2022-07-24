4965e30bd9c3a0e6a44b5a87a1e4ac3aa4f036d1
Simón Oroño <simonorono@protonmail.com>
2022-07-24 13:05:58 -0400

    Reduce size of "add" button

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -3,7 +3,7 @@ import { PlusIcon } from '@heroicons/react/outline'
 
 export default function AddButton() {
   return (
-    <div className="fixed right-4 bottom-4 h-16 w-16 rounded-full bg-black p-2">
+    <div className="fixed right-4 bottom-4 h-12 w-12 rounded-full bg-black p-2">
       <Link to="form">
         <PlusIcon className="text-white" />
       </Link>
