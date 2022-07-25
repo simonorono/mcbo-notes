6accbd7ed4f983197f361e6d8e83e4cc4e62670d
Simón Oroño <simonorono@protonmail.com>
2022-07-24 23:04:47 -0400

    Create action to set all notes in the note slice
    It will be useful for updating the notes list after it has been
    modified.

--- src/store/noteSlice.ts
+++ src/store/noteSlice.ts
@@ -12,12 +12,15 @@ const noteSlice = createSlice({
   name: 'notes',
   initialState,
   reducers: {
+    setAllNotes(state, action: PayloadAction<Note[]>) {
+      state.all = action.payload
+    },
     saveNote(state, action: PayloadAction<Note>) {
       state.all = [...state.all, action.payload]
     },
   },
 })
 
-export const { saveNote } = noteSlice.actions
+export const { saveNote, setAllNotes } = noteSlice.actions
 
 export default noteSlice.reducer
