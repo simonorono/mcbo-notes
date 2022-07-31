91523317f03d6433c1d6a68e586b11c063e80504
Simón Oroño <simonorono@protonmail.com>
2022-07-31 10:38:50 -0400

    Use Inter font through FontSource

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
