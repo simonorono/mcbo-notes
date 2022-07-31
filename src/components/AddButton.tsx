35b4d390830c6deed93f7f00bf09a44d6f3f96ca
Simón Oroño <simonorono@protonmail.com>
2022-07-31 11:36:25 -0400

    Add accessible name to add button

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -5,6 +5,7 @@ export default function AddButton() {
   return (
     <Link
       className="fixed right-4 bottom-4 inline-block h-12 w-12 rounded-full bg-black p-2"
+      title="Add new note"
       to="form"
     >
       <PlusIcon className="text-white" />
